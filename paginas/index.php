d60d16275217850e39eea5e80cf055eea42a6d90
s2carloss2 <carloscarloshenrique@hotmail.com>
2020-10-16 10:00:25 -0300

    arquivobase

--- paginas/index.php
+++ paginas/index.php
@@ -1,18 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-Br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documentos 02</title>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 </head>
 
 <body>
-    
+    <div class="container">
+
+        <div class="alert alert-primary" role="alert">
+            <h1 class="text-center display-1">companheirismo</h1>
+        </div>
+
+        <div class="alert alert-secondary" role="alert">
+            <h1 class="text-center display-2">lealdade</h1>
+        </div>
+
+        <div class="alert alert-success" role="alert">
+            <h1 class="text-center display-3">confiança</h1>
+        </div>
+
+        <div class="alert alert-warning" role="alert">
+            <h1 class="text-center display-4">união</h1>
+        </div>
+
+
+
+        <p class="lead">
+        </p> Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+        <hr>
+
+    </div>
+
+
+
+    <div class="container">
+
+        <footer>
+            
+            <div class="alert alert-success" role="alert">
+                <h4 class="text----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                -center alert-heading">Muito bem!</h4>
+                <p>Aêêê! Você conseguiu ler essa mensagem de alerta. Esse texto vai ter quer se extender um pouquinho pra você conseguir ver como o espaçamento dentro de um alerta funciona.</p>
+                <hr>
+                <p class="mb-0">Sempre que precisar, use utilitários de margem para manter as coisas perfeitas.</p>
+            </div>
+        </footer>
+    </div>
 
 
 
 
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
